5bcdebe5112565be2f052eea1990d49a4b2c36db
dulinshun <duls@shinemo.com>
2021-10-26 11:27:44 +0800

    Initial Commit

---
+++ H5Plugins/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  H5Plugins
+//
+//  Created by 杜林顺 on 2021/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
